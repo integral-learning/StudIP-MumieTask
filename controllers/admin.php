65420154a72d1c5f535fe59939d5e2d8f7d701dd
Tobias Goltz <tobias.goltz@integral-learning.com>
2020-02-07 12:22:02 +0100

    17216 implemented server-course-task structure

--- controllers/admin.php
+++ controllers/admin.php
@@ -1,6 +1,6 @@
 <?php 
 require_once('app/controllers/plugin_controller.php');
-require_once('public/plugins_packages/integral-learning/MumieTask/models/serverStructure/MumieServerInstance.php');
+require_once('public/plugins_packages/integral-learning/MumieTaskPlugin/models/serverStructure/MumieServerInstance.php');
 
 // TODO: Make sure that only admins have access
 
--- models/serverStructure/MumieServerInstance.php
+++ models/serverStructure/MumieServerInstance.php
@@ -1,14 +1,48 @@
 <?php
 
-class MumieServerInstance {
+
+require_once('public/plugins_packages/integral-learning/MumieTaskPlugin/models/serverStructure/MumieCourse.php');
+require_once('public/plugins_packages/integral-learning/MumieTaskPlugin/models/serverStructure/MumieProblem.php');
+require_once('public/plugins_packages/integral-learning/MumieTaskPlugin/models/serverStructure/MumieTag.php');
+class MumieServerInstance implements \JsonSerializable{
     private $server;
     /**
      * This is used as parameter when requesting available courses and tasks.
      */
     const MUMIE_JSON_FORMAT_VERSION = 2;
 
+    /**
+     * Primary key for db entry
+     * @var int
+     */
+    private $id;
+    /**
+     * Server URL
+     * @var string
+     */
+    private $url_prefix;
+    /**
+     * A human-readable name for a server configuration
+     * @var string
+     */
+    private $name;
+
+    /**
+     * All courses that are available on the server
+     * @var mumie_course[]
+     */
+    private $courses;
+
+    /**
+     * All languages that are available on the server
+     * @var string[]
+     */
+    private $languages = array();
+
     function __construct($server) {
         $this->server = $server;
+        $this->name = $server->name;
+        $this->url_prefix = $server->url_prefix;
     }
 
     /**
@@ -35,5 +69,42 @@ class MumieServerInstance {
      */
     public function isValidMumieServer() {
         return $this->getCoursesAndTasks()->courses != null;
+    }
+
+    /**
+     * Get and set the latest tasks and courses from the MUMIE server
+     */
+    public function loadStructure() {
+        PageLayout::postMessage(MessageBox::success("serverinstance: loading structure"));
+
+        $coursesandtasks = $this->getCoursesAndTasks();
+        $this->courses = [];
+        if ($coursesandtasks) {
+            foreach ($coursesandtasks->courses as $course) {
+                array_push($this->courses, new MumieCourse($course));
+            }
+        }
+        $this->collectLanguages();
+    }
+
+    /**
+     * Collect and set a list of all languages that are available on this MUMIE server
+     */
+    private function collectLanguages() {
+        $langs = [];
+        foreach ($this->courses as $course) {
+            array_push($langs, ...$course->getLanguages());
+        }
+        $this->languages = array_values(array_unique($langs));
+    }
+
+    /**
+     * Necessary to encode this object as json.
+     * @return mixed
+     */
+    public function jsonSerialize() {
+        $vars = get_object_vars($this);
+
+        return $vars;
     }
 }
