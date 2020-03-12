8eea72cbe76ad66f268ee2c61fa2bfc3ebac0fae
Tobias Goltz <tobias.goltz@integral-learning.com>
2020-03-12 10:29:45 +0100

    fixed iFrame for embedded view being too small

--- views/task/index.php
+++ views/task/index.php
@@ -1,16 +1,16 @@
+<?php if ($task->launch_container == 0) : ?>
 <section class="contentbox">
     <section>
         <header>
             <h1><?= dgettext("MumieTaskPlugin", "Inhalt") ?></h1>
         </header>
-        <?php if ($task->launch_container == 0) : ?>
         <a href=<?= PluginEngine::getLink("MumieTaskPlugin", array("task_id" => $task->task_id), 'task/launch'); ?>
             target="_blank" class="button"><?=dgettext("MumieTaskPlugin", "Anzeigen");?></a>
-        <?php else : ?>
-        <iframe width='90%' height='100%'
-            src=<?= PluginEngine::getLink("MumieTaskPlugin", array("task_id" => $task->task_id), 'task/launch'); ?>
-            webkitallowfullscreen mozallowfullscreen allowfullscreen>
-        </iframe>
-        <?php endif;?>
     </section>
 </section>
+<?php else : ?>
+<iframe width='90%' height='100%'
+    src=<?= PluginEngine::getLink("MumieTaskPlugin", array("task_id" => $task->task_id), 'task/launch'); ?>
+    webkitallowfullscreen mozallowfullscreen allowfullscreen>
+</iframe>
+<?php endif;?>
